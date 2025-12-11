f7d29be9983b85fa012fafcf3df682984bc20405
Michael Ballantyne <michael.ballantyne@gmail.com>
2025-12-11 12:25:33 -0500

    a couple more exercises

---
+++ syntax-case/README.md
@@ -0,0 +1,9 @@
+## syntax-case
+
+The `syntax-case` system was the first to effectively integrate procedural macros, hygiene, and a pattern matching and templating language.
+
+Pages 1-6 of "Syntactic Abstraction: The syntax-case expander" provides the best introduction to the system and the problems it is trying to solve (https://guenchi.github.io/Scheme/doc/Syntactic%20Abstraction%20the%20Syntax-Case%20Expander.pdf).
+
+Then, page 9 onwards of "Syntactic Abstraction in Scheme" provides the best explanation of the hygiene algorithm (https://scholarworks.iu.edu/iuswrrest/api/core/bitstreams/3f729d08-c698-4430-9d04-6a782c5b9300/content).
+
+[reading-exercises.rkt](reading-exercises.rkt) provides some examples to work through.
--- syntax-case/reading-exercises.rkt
+++ syntax-case/reading-exercises.rkt
@@ -33,13 +33,19 @@
   (let ([t2 #f])
     〚(if t2 t2 t)〛))
 
-
 ;;
 ;; Exercises
 ;;
 
 
-;; 1. Step through the hygienic expansion of this program:
+;; 1. Page 13 of "Syntactic Abstraction in Scheme" presents the `mark` and `subst` constructors
+;;    and the `resolve` operation. Using these constructors, what is the representation of the
+;;    `t` identifier that originated on the `(or #f t)` expression, after all of the marks
+;;    and substitutions from the expansion steps are applied to it?
+
+;; 2. Step through the `resolve` operation on the identifier from (1).
+
+;; 3. Step through the hygienic expansion of this program, like I did above for the `or` program:
 
 〚(let ([add1 (lambda (x) (+ x 1))])
   (letrec-syntax ([increment
@@ -50,7 +56,7 @@
     (let ([add1 0])
       (increment add1))))〛
 
-;; 2. The first hygiene algorithm, from "Hygienic Macro Expansion"
+;; 4. The first hygiene algorithm, from "Hygienic Macro Expansion"
 ;;    by Kohlbecker, Friedman, Felleisen, and Duba is simpler than
 ;;    the syntax-case algorithm. Rather than interleave the processes
 ;;    of marking and substituting, it marks while expanding but leaves the
