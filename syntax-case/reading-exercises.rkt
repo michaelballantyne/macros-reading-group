3481ff86097a1a963f381fc8b40498d451ecb386
Michael Ballantyne <michael.ballantyne@gmail.com>
2025-12-11 11:37:03 -0500

    add exercise for syntax-case

---
+++ syntax-case/reading-exercises.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+;;
+;; A worked example
+;;
+
+;; Below I step through an expansion of the `or` macro,
+;; showing the substitution steps performed for binding forms
+;; and the marking steps performed for macro applications.
+;;
+;; Syntax outside of the〚〛brackets is fully-expanded, while
+;; syntax within them is not yet expanded. The notation `tᵐ¹`
+;; indicates an identifier with symbol `t` and one mark `m1`.
+
+(define-syntax or
+  (lambda (stx)
+    (syntax-case stx ()
+      [( e1 e2)
+       #'(let ([t e1])
+           (if t t e2))])))
+
+〚(let ([t #t])
+   (or #f t))〛
+;; -> substitute let binder
+(let ([t1 #t])
+  〚(or #f t1)〛)
+;; -> transformer application marks introduced names
+(let ([t1 #t])
+  〚(let ([tᵐ¹ #f])
+     (if tᵐ¹ tᵐ¹ t))〛)
+;; -> substitute let binder (marked names are distinct from unmarked names when substituting)
+(let ([t1 #t])
+  (let ([t2 #f])
+    〚(if t2 t2 t)〛))
+
+
+;;
+;; Exercises
+;;
+
+
+;; 1. Step through the hygienic expansion of this program:
+
+〚(let ([add1 (lambda (x) (+ x 1))])
+  (letrec-syntax ([increment
+                   (lambda (x)
+                     (syntax-case x ()
+                       [(_ x)
+                        #'(set! x (add1 x))]))])
+    (let ([add1 0])
+      (increment add1))))〛
+
+;; 2. The first hygiene algorithm, from "Hygienic Macro Expansion"
+;;    by Kohlbecker, Friedman, Felleisen, and Duba is simpler than
+;;    the syntax-case algorithm. Rather than interleave the processes
+;;    of marking and substituting, it marks while expanding but leaves the
+;;    task of substituting a fresh name for each binder until a final step
+;;    when expansion is complete. If a name is not substituted, any marks
+;;    on it are dropped, as in the `resolve` function in the syntax-case
+;;    algorithm.
+;;
+;;    Try out this approach on the `increment` program---what goes wrong?
+
